551c49fc1a984f26201823a473e670e7753e45e5
Ana <anahaydn3@gmail.com>
2020-06-16 20:47:40 -0300

    verificando a funcionalidade

---
+++ a.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+		$('#bsobre').click(function(){
+			$('#sobre').css("display", "block").addClass('ativo')
+			$('#profes, #cal, #ctt').css("display", "none")
+			});
+
+		$('#bprofes').click(function(){
+			$('#profes').css("display", "block")
+			$('#sobre, #cal, #ctt').css("display", "none")
+			});
+		$('#bcal').click(function(){
+			$('#cal').css("display", "block")
+			$('#profes, #sobre, #ctt').css("display", "none")
+			});
+		$('#bctt').click(function(){
+			$('#ctt').css("display", "block")
+			$('#profes, #cal, #sobre').css("display", "none")
+			});
+		$("#menu").hover(function(){
+			$("#aaa, .menu").animate({width: "200px"},500)},
+			function(){
+			$("#aaa, .menu").animate({width: "100%"},500)});
+		$(".menu").hover(function(){
+				$(this).animate({textIndent:"8px", width:"200px", fontSize: "16px",},500)},
+				function(){
+					$(this).animate({textIndent:"0px", width:"100%", fontSize: "14px"},500)})
+			
+		$("#tsobre").click(function(){
+			$("#psobre").toggle()
+		})
+		$("#tcampo").click(function(){
+			$("#pcampo").toggle()
+		})
+		$("#tperfil").click(function(){
+			$("#pperfil").toggle()
+		})
+		$("#tmerc").click(function(){
+			$("#pmerc").toggle()
+		})
+		$("#tdur").click(function(){
+			$("#pdur").toggle()
+		})					
+})
